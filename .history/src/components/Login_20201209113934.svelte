dda7b4d6f0459d42f0d07cf60ee5cd88658d08c8
fabezio <fabezio@outlook.fr>
2020-12-09 12:07:11 +0100

    factorisation login & creation closeapp

---
+++ .history/src/components/Login_20201209113934.svelte
@@ -0,0 +1,36 @@
+<script>
+  import TopBar from "./Topbar.svelte";
+</script>
+
+<Topbar class="nav justify-content-between">
+  <li class="nav-item brand">Projet</li>
+  <li
+    type="button"
+    class=" nav-item btn-close"
+    on:click={closeApp}
+    aria-label="Close" />
+  <!-- <slot /> -->
+</Topbar>
+<!-- <Topbar>
+  <div class="close">&times;</div>
+</Topbar> -->
+<div class="panel">
+  <h1>S'identifier</h1>
+  <p>ou <a href="#">s'inscrire sur <span class="brand">Projet</span></a></p>
+  <form action="">
+    <input
+      class="form-control"
+      type="email"
+      bind:value={email}
+      placeholder="E-mail" />
+    <input
+      class="px-2"
+      type="password"
+      bind:value={password}
+      placeholder="Mot de passe" />
+    <a href="users" class="bg-blue" type="submit">S'identifier</a>
+  </form>
+  <a id="forgotten-password" href="#">Mot de passe oublié?</a>
+  <button type="submit" on:click={checkUser} class="bg-blue">S'identifier avec
+    l'empreinte digitale</button>
+</div>
---
+++ .history/src/components/Login_20201209114823.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Topbar from "./Topbar.svelte";
+  const dispatch = createEventDispatcher();
+  let email = "";
+  let password = "";
+</script>
+
+<Topbar class="nav justify-content-between">
+  <li class="nav-item brand">Projet</li>
+  <li
+    type="button"
+    class=" nav-item btn-close"
+    on:click={() => dispatch('closeapp')}
+    aria-label="Close" />
+  <!-- <slot /> -->
+</Topbar>
+<!-- <Topbar>
+  <div class="close">&times;</div>
+</Topbar> -->
+<div class="panel">
+  <h1>S'identifier</h1>
+  <p>ou <a href="#">s'inscrire sur <span class="brand">Projet</span></a></p>
+  <form action="">
+    <input
+      class="form-control"
+      type="email"
+      bind:value={email}
+      placeholder="E-mail" />
+    <input
+      class="px-2"
+      type="password"
+      bind:value={password}
+      placeholder="Mot de passe" />
+    <a
+      href="users"
+      class="btn-lg btn-primary d-block text-center my-2"
+      type="submit">S'identifier</a>
+  </form>
+  <a id="forgotten-password" href="#">Mot de passe oublié?</a>
+  <button
+    type="submit"
+    on:click={checkUser}
+    class="btn-lg btn-primary d-block my-2">S'identifier avec l'empreinte
+    digitale</button>
+</div>
